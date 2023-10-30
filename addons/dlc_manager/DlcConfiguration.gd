ad7d05854aaaec821788ac82d9089341100a7f82
Rafael Guerreiro <rguerreiro@kidsbanner.ca>
2023-10-29 18:57:49 -0700

    Structuring the project

--- addons/dlc_manager/DlcConfiguration.gd
+++ addons/dlc_manager/DlcConfiguration.gd
@@ -1,31 +1,12 @@
 extends Resource
 class_name DlcConfiguration
 
-# The config file url & location.
-# An example of the config file:
-# {
-#   "config_url": "https://mygame.com/dlc_config.json",
-#   "dlc": [
-#     {
-#       "name": "DLC1",
-#       "url": "https://mygame.com/dlc1.pck",
-#		"checksum": "1234567890abcdef1234567890abcdef12345678",
-#       "compat_versions": ["1.0", "2.0"] // DLC1 works for both 1.0 and 2.0
-#     },
-#     {
-#       "name": "DLC2",
-#       "url": "https://mygame.com/dlc2.zip",
-#		"checksum": "1234567890abcdef1234567890abcdef12345678",
-#       "compat_versions": ["1.0"]
-#     },
-#     {
-#       "name": "DLC3",
-#       "url": "https://mygame.com/dlc3.pck",
-#		"checksum": "1234567890abcdef1234567890abcdef12345678",
-#       "compat_versions": ["2.0"]
-#     }
-#   ]
-# }
+# The location of the configuration file
+@export var config_location: String = "res://dlc/dlc_config.json"
 
-@export var config_location: String = "res://dlc_config.json"
+# The url of the configuration
 @export var config_url: String = "https://mygame.com/dlc_config.json"
+
+# The location of the dlc download files
+@export var dlc_download_location: String = "user://dlcs/"
+
